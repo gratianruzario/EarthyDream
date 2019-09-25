fbf0a7f7e76bd875d11f65edcde149d836bf085e
flavglen <flavglen@gmail.com>
2019-09-25 20:58:03 +0530

    fixed issue

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
-import { Observable, from, of, combineLatest, concat } from 'rxjs';
+import { Observable, from, of, combineLatest, concat, forkJoin } from 'rxjs';
 import { switchMap, map, mergeMap, toArray, take, mergeAll, concatMap, tap, scan, flatMap, first, last } from 'rxjs/operators';
 import { currentId } from 'async_hooks';
 
@@ -15,38 +15,42 @@ export class ProductListComponent implements OnInit {
   constructor( private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.getAllProducts3();
+    this.getAllProducts();
   }
+
 
   getAllProducts() {
     this.db.collection('products').snapshotChanges().pipe(
-      map(actions => actions.map((action, i) => {
 
-        const data = action.payload.doc.data();
-        const id = action.payload.doc.id;
-
-        return { id, ...data  };
-      })),
+      mergeMap(products => {
+        const obs = products.map( product => {
+          return this.getProductImages(product.payload.doc.id).pipe(map(images => {
+             return Object.assign(product, images);
+             }
+            ));
+      });
+        return forkJoin(obs);
+      }),
 
     ).subscribe(products => {
-      console.log(products);
-      this.productList = products;
+
+       console.log(products);
+       this.productList = products;
+    }, error => {
+      console.log(error);
     });
 
 
   }
 
-  getAllProducts3() {
+  getAllProducts_old() {
     this.db.collection('products').snapshotChanges().pipe(
       mergeMap(product => product),
       mergeMap(product => this.getProductImages(product.payload.doc.id), (productObj , productImage) => {
          return  {...productObj.payload.doc.data(), ...productImage };
        }),
+       toArray()
 
-         scan((acc, value) => {
-      acc.push(value);
-      return acc;
-  }, []),
     ).subscribe(products => {
 
        console.log(products);
@@ -62,10 +66,7 @@ export class ProductListComponent implements OnInit {
   }
 
   getProductImages(id): Observable<any> {
-
-        return this.db.doc(`productImages/${ id}`).valueChanges().pipe(
-          map(ridePage => ridePage),
-        );
+        return this.db.doc(`productImages/${ id}`).valueChanges().pipe(take(1));
   }
 
 }
