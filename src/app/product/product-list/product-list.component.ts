6f7830c11918f800eaebca856c7bd12876883b7e
flavglen <flavglen@gmail.com>
2019-09-25 08:51:48 +0530

    product list

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { Observable, from, of, combineLatest, concat } from 'rxjs';
+import { switchMap, map, mergeMap, toArray, take, mergeAll, concatMap, tap, scan, flatMap, first, last } from 'rxjs/operators';
+import { currentId } from 'async_hooks';
 
 @Component({
   selector: 'app-product-list',
@@ -10,10 +11,11 @@ import { switchMap, map } from 'rxjs/operators';
 })
 export class ProductListComponent implements OnInit {
   productList: any[] = [];
+  productImages = {};
   constructor( private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.getAllProducts();
+    this.getAllProducts3();
   }
 
   getAllProducts() {
@@ -23,13 +25,47 @@ export class ProductListComponent implements OnInit {
         const data = action.payload.doc.data();
         const id = action.payload.doc.id;
 
-        return { id, ...data };
-      }))
+        return { id, ...data  };
+      })),
+
     ).subscribe(products => {
+      console.log(products);
       this.productList = products;
     });
 
 
+  }
+
+  getAllProducts3() {
+    this.db.collection('products').snapshotChanges().pipe(
+      mergeMap(product => product),
+      mergeMap(product => this.getProductImages(product.payload.doc.id), (productObj , productImage) => {
+         return  {...productObj.payload.doc.data(), ...productImage };
+       }),
+
+         scan((acc, value) => {
+      acc.push(value);
+      return acc;
+  }, []),
+    ).subscribe(products => {
+
+       console.log(products);
+       this.productList = products;
+    }, error => {
+      console.log(error);
+    });
+
+  //   scan((acc, value) => {
+  //     acc.push(value);
+  //     return acc;
+  // }, []),
+  }
+
+  getProductImages(id): Observable<any> {
+
+        return this.db.doc(`productImages/${ id}`).valueChanges().pipe(
+          map(ridePage => ridePage),
+        );
   }
 
 }
